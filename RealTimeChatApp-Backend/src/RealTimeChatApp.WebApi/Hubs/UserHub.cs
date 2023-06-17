36639fc891f8c54462a4bcbff40e6fe2fef432d4
nmerveb <nmerveb@gmail.com>
2023-06-17 20:25:26 +0300

    SignalR Hubs added.

---
+++ RealTimeChatApp-Backend/src/RealTimeChatApp.WebApi/Hubs/UserHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeChatApp.WebApi.Hubs
+{
+    public class UserHub : Hub
+    {
+        //task lis message mesajlari doner api olmadan 
+        private static List<string> userList = new List<string>();
+        private static List<Message> messageList = new List<Message>();
+
+        public List<string> GetUserList()
+        {
+            return userList;
+        }
+
+        public void AddUser(string username)
+        {
+            userList.Add(username);
+            Clients.All.SendAsync("UserAdded", username);
+        }
+
+        public void DeleteUser(string username)
+        {
+            userList.Remove(username);
+            Clients.All.SendAsync("UserDeleted", username);
+        }
+
+        public List<Message> GetMessageList()
+        {
+            return messageList;
+        }
+
+        public void AddMessage(Message message)
+        {
+            message.CreatedOn = DateTime.Now;
+            messageList.Add(message);
+            Clients.All.SendAsync("MessageAdded", message);
+        }
+    }
+
+    public class Message
+    {
+        public string Username { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+
+}
+
+
+
--- RealTimeChatApp-Backend/src/RealTimeChatApp.WebApi/Program.cs
+++ RealTimeChatApp-Backend/src/RealTimeChatApp.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using RealTimeChatApp.WebApi.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Add Services
+
+builder.Services.AddSignalR();
+
 
 var app = builder.Build();
 
@@ -21,5 +29,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<UserHub>("/userhub");
 
 app.Run();
